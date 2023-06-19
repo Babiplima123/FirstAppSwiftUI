28ea0da94fe075f49684dba3b6381e9b2660fc4d
Barbara Lima <Babiplima123>
2023-06-19 17:59:54 -0300

    first commit

---
+++ profileapp01/profileapp01App.swift
@@ -0,0 +1,17 @@
+//
+//  profileapp01App.swift
+//  profileapp01
+//
+//  Created by Barbara Lima on 14/05/23.
+//
+
+import SwiftUI
+
+@main
+struct profileapp01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
